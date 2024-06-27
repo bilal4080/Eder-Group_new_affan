df880dd769becf0c0f9a1412a739bb7cae48d62e
Bilal <68295612+bilal4080@users.noreply.github.com>
2024-06-27 11:11:22 +0500

    GTAG

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import GlobalStateProvider from "@/components/GlobalStateProvider";
 import PricingPopUp from "@/components/cal_pricing/PricingPopUp";
 import Layout from "@/components/Layout/Layout";
-
+import Script from "next/script";
 const outfit = Outfit({subsets: ["latin"]});
 
 export const metadata = {
@@ -20,6 +20,17 @@ export const metadata = {
 export default function RootLayout({children}) {
     return (
         <html lang="de">
+            <head>
+                <Script async src="https://www.googletagmanager.com/gtag/js?id=G-TR230NE73T"></Script>
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${"G - TR230NE73T"}');
+                `}
+                </Script>
+            </head>
             <link rel="icon" href="/playstore.ico" sizes="any" />
             <body className={`${outfit.className} overflow-x-hidden`}>
                 <GlobalStateProvider>
