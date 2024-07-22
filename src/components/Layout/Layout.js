130d724cf877df0a047b0c5faa26f1ffc2eccd06
Bilal <68295612+bilal4080@users.noreply.github.com>
2024-07-22 14:22:53 +0500

    bddas

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,14 +2,14 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 
-const Layout = ({ children }) => {
-  return (
-    <>
-      <Header />
-      {children}
-      <Footer />
-    </>
-  );
+const Layout = ({children}) => {
+    return (
+        <>
+            <Header />
+            {children}
+            <Footer />
+        </>
+    );
 };
 
 export default Layout;
