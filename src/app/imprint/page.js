3de98f20a4f45bd328eb0da56c86ecc32ea5f23b
Bilal <68295612+bilal4080@users.noreply.github.com>
2024-07-30 22:22:46 +0500

    few

--- src/app/imprint/page.js
+++ src/app/imprint/page.js
@@ -8,12 +8,10 @@ const page = () => {
 
             <h2 className="text-xl font-semibold mt-4 data-translate">Legal Provider Identification:</h2>
             <p data-translate>EDER - Steel GmbH</p>
-            <p data-translate>
-                represented by the Managing Directors Mr. Richard Stoll, Mrs. Regina Stoll, Mr. Ronald Binder
-            </p>
-            <p data-translate>Alois - Senefelder - Allee 1</p>
-            <p data-translate>86153 Augsburg</p>
-            <p data-translate>Germany</p>
+            <p data-translate>represented by the Managing Directors Mr. Muhammad Affan Rana</p>
+            <p data-translate>Hindenburgstraße 14, 6356,</p>
+            <p data-translate>Neusäß,</p>
+            <p data-translate>Deutschland</p>
 
             <h2 data-translate className="text-xl font-semibold mt-4">
                 Contact
@@ -29,23 +27,23 @@ const page = () => {
             <h2 data-translate className="text-xl font-semibold mt-4">
                 VAT ID
             </h2>
-            <p data-translate>VAT ID No.: DE 813902859</p>
+            {/* <p data-translate>VAT ID No.: DE 8</p> */}
 
             <h2 data-translate className="text-xl font-semibold mt-4">
                 Commercial Register
             </h2>
-            <p data-translate>registered in the commercial register of the district court of Augsburg</p>
-            <p data-translate>Commercial Register Number HRB 20362</p>
+            <p data-translate>registered in the commercial register of the district court of Stuttgart</p>
+            <p data-translate>Commercial Register Number HRB 792948</p>
 
             <h2 data-translate className="text-xl font-semibold mt-4">
                 Data Protection Officer
             </h2>
-            <p data-translate>Data Protection Officer: Mrs. Isabell Stoll</p>
+            <p data-translate>Data Protection Officer: Mr. Muhammad Affan Rana</p>
 
             <h2 data-translate className="text-xl font-semibold mt-4">
                 FairCommerce
             </h2>
-            <p data-translate>We have been a member of the "FairCommerce" initiative since 18.04.2018.</p>
+
             <p>
                 For more information, please visit{" "}
                 <a href="http://www.fair-commerce.de" className="text-blue-500">
